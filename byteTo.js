81dc2ea5ccac5ce75178e53b6cd9bec6f3510ef1
Narek Hovhannisyan <narekgilmour@gmail.com>
2017-09-25 19:00:15 +0400

    refactoring

--- byteTo.js
+++ byteTo.js
@@ -1,10 +1,19 @@
-const byteTo = (byte, counter) => {
-  const arr = ['byte', 'kb', 'mb', 'tb']
-  if (Math.ceil(byte / 1024) >= 1024) {
-    return byteTo(byte / 1024, counter + 1)
-  } else {
-    console.log(byte, arr[counter])
+/**
+ * @param {number} byte - Byte to be converted.
+ * @return {object} - Object that contains converted byte size and measure unit.
+ * @description Converts byte into byte, kb, mb, gb, tb depending on size.
+ */
+const byteTo = (byte) => {
+  const UNITS = ['byte', 'kb', 'mb', 'gb', 'tb']
+  const getPowAndMult = (byte, counter = 0) => {
+    if (byte >= 1024) {
+      return getPowAndMult(byte / 1024, counter + 1)      
+    } else {
+      return {
+        size: Number(byte).toFixed(3),
+        unit: UNITS[counter]
+      }
+    }
   }
+  return getPowAndMult(byte)
 }
-
-byteTo(2048 * 2048, 0)
